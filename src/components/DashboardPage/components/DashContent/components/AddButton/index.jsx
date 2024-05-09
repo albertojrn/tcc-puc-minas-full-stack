74b8f92b11323186fefde08b64a8f839ab2900df
T0033SX <alberto.neto1@external.stellantis.com>
2024-05-09 19:38:06 -0300

    .

--- src/components/DashboardPage/components/DashContent/components/AddButton/index.jsx
+++ src/components/DashboardPage/components/DashContent/components/AddButton/index.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
 import { Button } from '@mui/material'
+import { useDashboardContext } from '../../../../../../contexts/DashboardContext'
 
 function AddButton({ page }) {
-  return <Button variant='contained'>Novo</Button>
+  const { setDashboardData } = useDashboardContext
+  function handleOnAddClick() {
+    setDashboardData({ openModal: true, modalPage: page })
+  }
+
+  return <Button variant='contained' onClick={handleOnAddClick}>Novo</Button>
 }
 
 export default AddButton
